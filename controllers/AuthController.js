f735e1c752acb1a62a58af2a36c8ab209ca5cceb
Joseph Abraham <ekurubal24@gmail.com>
2020-12-10 23:58:17 +0300

    Add auth register todo

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,21 @@
 exports.auth_register = (req, res) => {
     //TODO: Register func.
+    //req.body.firstName
+
+    const {firstName, lastName, email, password} = req.body;
+    console.log("Fields:",
+      firstName, 
+      lastName, 
+      email,
+      password
+    ); 
+
+    //TODO1:Validate the field
+    // TODO2:check already registered
+    //TODO3:crpyt password
+    //TODO4:save the user to DB
+
+
     res.send("Register Completed.");
   };
   
