40e2e1cfa4d74c6d0058e5b69cf370f748d8b3af
Joseph Abraham <ekurubal24@gmail.com>
2020-12-10 22:48:21 +0300

    add controller temps

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,22 +1,11 @@
-const express = require("express");
-
-const router = express.Router();
-
-const AuthController = require("../controllers/AuthController");
-// routes for /api/auth
-
-/**
- * @route   POST /api/auth/register
- * @desc    Register endpoint
- * @access  Public
- */
-router.post("/register", AuthController.auth_register);
-
-/**
- * @route   POST /api/auth/login
- * @desc    Login endpoint
- * @access  Private
- */
-router.post("/login", AuthController.auth_login);
-
-module.exports = router;
+exports.auth_register = (req, res) => {
+    //TODO: Register func.
+    res.send("Register Completed.");
+  };
+  
+  exports.auth_login = (req, res) => {
+    // TODO: Auth.
+    // TODO: Login func.
+    res.send("Login Completed");
+  };
+  
